de0d60eb4aad0bb4d91523bb39e62615425d2d3b
byebai95 <50128162+byebai95@users.noreply.github.com>
2022-04-24 22:37:27 +0800

    update
    update

--- HelloWorld/urls.py
+++ HelloWorld/urls.py
@@ -1,23 +1,25 @@
 from django.conf.urls import url
 from django.contrib import admin
-from django.urls import re_path
+from django.urls import re_path, path, include
 
 from . import views, search
 
 urlpatterns = [
 
-   url(r'^admin/', admin.site.urls),
+    url(r'^admin/', admin.site.urls),
 
-    # re_path(r'^hello/$', views.hello),
-    # re_path(r'^runoob/$', views.runoob),
-    # re_path(r'^script/$', views.script),
-    # re_path(r'^iftest/$', views.iftest),
-    # re_path(r'^fortest/$', views.fortest),
-    # re_path(r'^href/$', views.href),
-    #
-    # re_path(r'^search-form/$', search.search_form),
-    # re_path(r'^search/$', search.search),
-    #
-    # path('model/', include('TestModel.urls')),
+    re_path(r'^hello/$', views.hello),
+    re_path(r'^runoob/$', views.runoob),
+    re_path(r'^script/$', views.script),
+    re_path(r'^iftest/$', views.iftest),
+    re_path(r'^fortest/$', views.fortest),
+    re_path(r'^href/$', views.href),
+
+    re_path(r'^search-form/$', search.search_form),
+    re_path(r'^search/$', search.search),
+
+    path('model/', include('TestModel.urls')),
+
+    path('env/', views.envtest),
 
 ]
--- HelloWorld/views.py
+++ HelloWorld/views.py
@@ -1,6 +1,9 @@
+import os
+
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.template.defaulttags import register
+from environs import Env
 
 
 def hello(request):
@@ -31,3 +34,9 @@ def href(request):
     request.encoding = 'utf-8'
     dct = {'hrefStr': "<a href='https://www.runoob.com/'>点击跳转</a>"}
     return render(request, 'href.html', dct)
+
+
+def envtest(request):
+    env = Env()
+    host = env.str("HOST")
+    return render(request, 'env.html', {"HOST": host})
